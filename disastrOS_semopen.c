658ecb0e7164fc07bfe0570879ca9dc91492bd5f
Valerio <valeriosilvestro24@gmail.com>
2019-04-18 21:46:55 +0200

    allocation handled

--- disastrOS_semopen.c
+++ disastrOS_semopen.c
@@ -11,8 +11,30 @@ void internal_semOpen(){
   
   if (semid <0){
 	  disastrOS_debug("The semaphore id is invalid");
-	  running->syscall_retvalue = DSOS_SEMNEG;
+	  running->syscall_retvalue = DSOS_SEMNEGERROR;
 	  return;
   }
+
+  /*checking if a semaphore with the given id already exists*/
+  Semaphore* mySem = SemaphoreList_byId(&semaphores_list, semid); 
   
+  if (!mySem) {
+	  disastrOS_debug("Allocation of semaphore with id: %d\n", semid);
+	  /* it doesn't esist --> let's allocate*/
+	  mySem = Semaphore_alloc(semid,1);
+	  
+	  if (mySem == NULL){
+		  disastrOS_debug("Allocation failed\n");
+		  running->syscall_retvalue = DSOS_SEMALLOCERROR;
+		  return;
+	  }
+	  
+	  else{ disastrOS_debug("Successful allocation\n");}
+	  
+	  /* semaphore in the list*/
+	  List_insert(&semaphores_list, semaphore_list.last, (ListItem*) mySem);
+  }
+
+		  
+		     
 }
