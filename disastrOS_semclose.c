361e1b867eea0959cf70dedc3b41e514d6506173
Valerio <valeriosilvestro24@gmail.com>
2019-04-19 20:15:20 +0200

    3 hrs for 5 lines, to finish

--- disastrOS_semclose.c
+++ disastrOS_semclose.c
@@ -7,5 +7,15 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semClose(){
-  // do stuff :)
+	in sem_fd = running-> syscall_args[0];
+	/*checking if it exists*/
+	SemDescriptor* sem_des = SemDescriptorList_byFd(&(running->sem_descriptors), sem_fd);
+	if (!sem_des){
+		disastrOS_debug("There's no semaphore with the given fd\n");
+		running->syscall_retvalue = DSOS_NOSEMERROR;
+		return;
+	}
+	/*removing descriptor*/
+	sem_des = List_detach(&(running->sem_descriptors), (ListItem*) sem_fd);
+	
 }
