0264eb9c891b321f6a2c9555fa5e247a81dbba81
Valerio <valeriosilvestro24@gmail.com>
2019-04-24 15:51:57 +0200

    semclose almost completed

--- disastrOS_semclose.c
+++ disastrOS_semclose.c
@@ -7,7 +7,7 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semClose(){
-	in sem_fd = running-> syscall_args[0];
+	int sem_fd = running-> syscall_args[0];
 	/*checking if it exists*/
 	SemDescriptor* sem_des = SemDescriptorList_byFd(&(running->sem_descriptors), sem_fd);
 	if (!sem_des){
@@ -19,12 +19,12 @@ void internal_semClose(){
 	sem_des = List_detach(&(running->sem_descriptors), (ListItem*) sem_fd);
 	
 	/* <assert> displays an error message on stderr if the expression is FALSE*/
-	assert(des); 
+	assert(sem_des); 
 	
 	Semaphore* Sem = sem_des-> semaphore;
 	
 	/* removing descriptors from the semaphore*/
-	SemDescriptorPtr* sem_des_ptr = List_detach(&(Sem->descriptors, (ListItem*)(sem_des->ptr));
+	SemDescriptorPtr* sem_des_ptr = List_detach(&(Sem->descriptors), (ListItem*)(sem_des->ptr));
 	assert(sem_des_ptr);
 	
 	/*free*/
@@ -33,17 +33,17 @@ void internal_semClose(){
 	SemDescriptor_free(sem_des);
 	assert(sem_des);
 	
-	int dim = sem->descriptors.size;
+	int dim = Sem->descriptors.size;
 	
 	/*check if the semaphore is not used*/
 	if (dim == 0){
 		disastrOS_debug("semaphore with id %d will be deleted\n", Sem->id);
 		Sem = List_detach(&(semaphore_list), (ListItem*) Sem);
 		assert(Sem);
-		Semaphore_free(sem);
+		Semaphore_free(Sem);
 	}
 	
 	/*success code*/
 	running->syscall_retvalue = 0;
-	
+	return;
 }
