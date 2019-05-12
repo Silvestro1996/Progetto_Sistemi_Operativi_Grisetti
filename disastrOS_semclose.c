2381527487a51773d41418022274b51d73f9208b
Valerio <valeriosilvestro24@gmail.com>
2019-05-12 21:53:31 +0200

    some bugs fixed

--- disastrOS_semclose.c
+++ disastrOS_semclose.c
@@ -7,42 +7,45 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semClose(){
-	int sem_fd = running-> syscall_args[0];
+
+	int sem_fd = running->syscall_args[0];
+
 	/*checking if it exists*/
-	SemDescriptor* sem_des = SemDescriptorList_byFd(&(running->sem_descriptors), sem_fd);
-	if (!sem_des){
-		disastrOS_debug("There's no semaphore with the given fd\n");
-		running->syscall_retvalue = DSOS_NOSEMERROR;
+	SemDescriptor* sem_des = SemDescriptorList_byFd(&running->sem_descriptors, sem_fd);
+	if(!sem_des)
+	{
+		running->syscall_retvalue = DSOS_NOSEMERROR; //New error code
 		return;
 	}
+
+	//Get the semaphore associated to the descriptor
+	Semaphore* Sem = sem_des->semaphore;
+
 	/*removing descriptor from Process Control Block*/
-	sem_des = (SemDescriptor*) List_detach(&(running->sem_descriptors), 					 (ListItem*) sem_des);
-	
+	sem_des = (SemDescriptor*) List_detach(&running->sem_descriptors, (ListItem*)sem_des);
 	/* <assert> displays an error message on stderr if the expression is FALSE*/
-	assert(sem_des); 
-	
-	Semaphore* Sem = sem_des-> semaphore;
-	
+	assert(sem_des);
+
 	/* removing descriptors from the semaphore*/
-	SemDescriptorPtr* sem_des_ptr = (SemDescriptorPtr*) List_detach(&(Sem->descriptors), (ListItem*)(sem_des->ptr));
+	SemDescriptorPtr* sem_des_ptr = (SemDescriptorPtr*) List_detach(&Sem->descriptors, (ListItem*) sem_des->ptr);
 	assert(sem_des_ptr);
-	
+
 	/*free*/
 	SemDescriptorPtr_free(sem_des_ptr);
-	assert(sem_des_ptr);
 	SemDescriptor_free(sem_des);
-	assert(sem_des);
-	
-	int dim = Sem->descriptors.size;
-	
+	assert(sem_des_ptr || sem_des);
+
+	//int dim = Sem->descriptors.size;
 	/*check if the semaphore is not used*/
-	if (dim == 0){
-		disastrOS_debug("semaphore with id %d will be deleted\n", Sem->id);
-		Sem = (Semaphore*) List_detach(&(semaphores_list), (ListItem*) Sem);
+	if(Sem->descriptors.size == 0 && Sem->waiting_descriptors.size == 0)
+	{
+		printf("Semaphore with ID %d will be deleted\n", Sem->id);
+
+		Sem = (Semaphore*) List_detach(&semaphores_list, (ListItem*) Sem);
 		assert(Sem);
 		Semaphore_free(Sem);
 	}
-	
+
 	/*success code*/
 	running->syscall_retvalue = 0;
 	return;
